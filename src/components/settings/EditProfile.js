d35452082f3b752f16a8acd02bc70d3f75d4a0cd
lalukrish <lalukrishnavallil01@gmail.com>
2023-08-19 19:26:52 +0530

    feat:full name change otpion

--- src/components/settings/EditProfile.js
+++ src/components/settings/EditProfile.js
@@ -1,6 +1,7 @@
 import { TextField, Button, Grid } from '@mui/material';
 import React, { useState, useEffect } from 'react';
 import { useFormik } from 'formik';
+import axios from 'axios';
 
 import { useDispatch, useSelector } from 'react-redux';
 import { userProfile } from '../../redux/slices/userProfileSlice';
@@ -17,6 +18,29 @@ const EditProfile = () => {
 
   const userData = useSelector((state) => state.myprofile?.successMessage?.data?.user);
 
+  const updateProfile = async (formikValues) => {
+    console.log('submit');
+    const id = localStorage.getItem('USER_ID');
+
+    const response = await axios.put(
+      `${process.env.REACT_APP_NEXTTECH_DEV_URL}/edit-profile
+        `,
+      {
+        id,
+        full_name: formikValues.full_name,
+      },
+      {
+        headers: {
+          accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+    console.log('res');
+
+    return response;
+  };
+
   const formik = useFormik({
     enableReinitialize: true,
     initialValues: {
@@ -24,11 +48,18 @@ const EditProfile = () => {
       email: userData ? userData.email : '',
       user_name: userData ? userData.user_name : '', // Add this line for email
     },
-    // ...
+    onSubmit: async () => {
+      console.log('submit');
+
+      updateProfile(formik.values).then((response) => {
+        console.log('hi', response);
+      });
+    },
   });
+  console.log('hi');
 
   return (
-    <>
+    <form onSubmit={formik.handleSubmit}>
       <Grid container spacing={2}>
         <Grid item xs={12}>
           <TextField
@@ -46,6 +77,7 @@ const EditProfile = () => {
             fullWidth
             onChange={formik.handleChange}
             value={formik.values ? formik.values.email : ''}
+            disabled
           />
         </Grid>
         <Grid item xs={12}>
@@ -55,16 +87,17 @@ const EditProfile = () => {
             fullWidth
             onChange={formik.handleChange}
             value={formik.values ? formik.values.user_name : ''}
+            disabled
           />
         </Grid>
         {/* ... Other fields */}
         <Grid item xs={12}>
-          <Button variant="contained" color="primary" fullWidth sx={{ height: '50px' }}>
+          <Button variant="contained" color="primary" fullWidth sx={{ height: '50px' }} type="submit">
             Save Changes
           </Button>
         </Grid>
       </Grid>
-    </>
+    </form>
   );
 };
 
