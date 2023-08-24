01ad2106e01ea52fa9dfb94b0655137ac13cbb41
theetha1608 <theerthakunjusss@gmail.com>
2023-08-24 23:50:13 +0530

    addpost1

--- src/pages/BlogPage.js
+++ src/pages/BlogPage.js
@@ -2,6 +2,9 @@ import { Helmet } from 'react-helmet-async';
 // @mui
 import { Grid, Button, Container, Stack, Typography } from '@mui/material';
 // components
+import BlogPageResults from '../components/blog/blog-page';
+import SimpleCard from '../components/blog/add-posts';
+
 import Iconify from '../components/iconify';
 import { BlogPostCard, BlogPostsSort, BlogPostsSearch } from '../sections/@dashboard/blog';
 // mock
@@ -25,26 +28,9 @@ export default function BlogPage() {
       </Helmet>
 
       <Container>
-        <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
-          <Typography variant="h4" gutterBottom>
-            Blog
-          </Typography>
-          <Button variant="contained" startIcon={<Iconify icon="eva:plus-fill" />}>
-            New Post
-          </Button>
-        </Stack>
-
-        <Stack mb={5} direction="row" alignItems="center" justifyContent="space-between">
-          <BlogPostsSearch posts={POSTS} />
-          <BlogPostsSort options={SORT_OPTIONS} />
-        </Stack>
-
-        <Grid container spacing={3}>
-          {POSTS.map((post, index) => (
-            <BlogPostCard key={post.id} post={post} index={index} />
-          ))}
-        </Grid>
-      </Container>
+        <SimpleCard/>
+        <BlogPageResults/>
+    </Container>
     </>
   );
 }
