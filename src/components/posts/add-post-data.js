5f01b1508b77af41af6c4bf6ba6a80cbf5448643
theetha1608 <theerthakunjusss@gmail.com>
2023-08-26 22:03:02 +0530

    posts

--- src/components/posts/add-post-data.js
+++ src/components/posts/add-post-data.js
@@ -1,13 +1,70 @@
-import { TextField } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
+import { TextField, Button, Card, CardContent, CardMedia } from '@mui/material';
+import axios from 'axios';
 
 const AddPostData = () => {
+  const [title, setTitle] = useState('');
+  const [location, setLocation] = useState('');
+  const [image, setImage] = useState(null);
+
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const handleLocationChange = (e) => {
+    setLocation(e.target.value);
+  };
+
+  const handleImageChange = (e) => {
+    setImage(e.target.files[0]);
+  };
+
+  const handlePost = async () => {
+    const formData = new FormData();
+    formData.append('title', title);
+    formData.append('location', location);
+    formData.append('image', image);
+
+    try {
+      const response = await axios.post('/api/post', formData);
+      console.log('Post successful', response.data);
+      setTitle('');
+      setLocation('');
+      setImage(null);
+    } catch (error) {
+      console.error('Error posting', error);
+    }
+  };
+
   return (
-    <>
-      <form>
-        <TextField placeholder="post title" />
-      </form>
-    </>
+    <Card>
+      {image && (
+        <CardMedia
+          component="img"
+          alt="Chosen Image"
+          height="300"
+          image={URL.createObjectURL(image)}
+        />
+      )}
+      <CardContent>
+        <TextField
+          label="Title"
+          fullWidth
+          value={title}
+          onChange={handleTitleChange}
+        />
+        <TextField
+          label="Location"
+          fullWidth
+          value={location}
+          onChange={handleLocationChange}
+        />
+        <input type="file" accept="image/*" onChange={handleImageChange} />
+        <Button variant="contained" color="primary" onClick={handlePost}>
+          Post
+        </Button>
+      </CardContent>
+    </Card>
   );
 };
 
