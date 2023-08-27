aa1d703fabff41302981e90871fc21fe9da48d27
lalukrish <lalukrishnavallil01@gmail.com>
2023-08-27 20:15:33 +0530

    fix

--- src/components/posts/add-post-data.js
+++ src/components/posts/add-post-data.js
@@ -3,6 +3,7 @@ import { TextField, Button, Card, CardContent, CardMedia } from '@mui/material';
 import axios from 'axios';
 
 const AddPostData = () => {
+  const userId = localStorage.getItem('USER_ID');
   const [title, setTitle] = useState('');
   const [location, setLocation] = useState('');
   const [image, setImage] = useState(null);
@@ -21,12 +22,13 @@ const AddPostData = () => {
 
   const handlePost = async () => {
     const formData = new FormData();
+    formData.append('id', userId);
     formData.append('title', title);
     formData.append('location', location);
     formData.append('image', image);
 
     try {
-      const response = await axios.post('/api/post', formData);
+      const response = await axios.post(`${process.env.REACT_APP_NEXTTECH_DEV_URL}/create_post`, formData);
       console.log('Post successful', response.data);
       setTitle('');
       setLocation('');
@@ -38,27 +40,10 @@ const AddPostData = () => {
 
   return (
     <Card>
-      {image && (
-        <CardMedia
-          component="img"
-          alt="Chosen Image"
-          height="300"
-          image={URL.createObjectURL(image)}
-        />
-      )}
+      {image && <CardMedia component="img" alt="Chosen Image" height="300" image={URL.createObjectURL(image)} />}
       <CardContent>
-        <TextField
-          label="Title"
-          fullWidth
-          value={title}
-          onChange={handleTitleChange}
-        />
-        <TextField
-          label="Location"
-          fullWidth
-          value={location}
-          onChange={handleLocationChange}
-        />
+        <TextField label="Title" fullWidth value={title} onChange={handleTitleChange} />
+        <TextField label="Location" fullWidth value={location} onChange={handleLocationChange} />
         <input type="file" accept="image/*" onChange={handleImageChange} />
         <Button variant="contained" color="primary" onClick={handlePost}>
           Post
