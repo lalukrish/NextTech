1d9aebb7b44112e47a62088518a4f8de4ac84b58
lalukrish <lalukrishnavallil01@gmail.com>
2023-09-09 14:13:05 +0530

    feat:like and unlike

--- src/components/blog/blog-page.js
+++ src/components/blog/blog-page.js
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import { Avatar, Button, Card, CardContent, CardActions, CardHeader, IconButton, Typography } from '@mui/material';
-import { Favorite as FavoriteIcon,FavoriteBorderOutlined as FavoriteBorderOutlinedIcon, ChatBubbleOutline as ChatBubbleOutlineIcon, AddComment } from '@mui/icons-material';
+import {
+  Favorite as FavoriteIcon,
+  FavoriteBorderOutlined as FavoriteBorderOutlinedIcon,
+  ChatBubbleOutline as ChatBubbleOutlineIcon,
+  AddComment,
+} from '@mui/icons-material';
 import axios from 'axios';
 import { useSelector } from 'react-redux';
 import MyPostCardModal from './blog-post-fullVIew-modal';
 
-
-
-
 const BlogPageResults = () => {
+  const userid = localStorage.getItem('USER_ID');
   const userName = useSelector((state) => state.myprofile?.successMessage?.data?.user?.full_name);
 
   const userProfileImage = useSelector((state) => state.myprofilepic?.successMessage?.data?.data?.profile_image_url);
@@ -55,23 +58,65 @@ const BlogPageResults = () => {
   };
 
   console.log('postid______>', postId);
-  const [postLikes, setPostLikes] = useState(0); 
-const [isLiked, setIsLiked] = useState(false); 
 
-const handleLikePost = () => {
-  if (isLiked) {
-    setPostLikes(postLikes - 1);
-    setIsLiked(false);
+  const handleSetLike = (postid) => {
+    const config = {
+      method: 'post',
+      url: `${process.env.REACT_APP_NEXTTECH_DEV_URL}/comments/add-like`,
+      data: {
+        userid,
+        postid,
+      },
+      headers: {
+        // Add any headers you need here
+      },
+    };
+    axios(config).then((response) => {
+      const data = response.data;
+      console.log('all post', data);
+      setIsLiked(true);
+      handleAllPosts();
+    });
+  };
 
-  } else {
-    setPostLikes(postLikes + 1);
-    setIsLiked(true);
-  }
-};
+  const handleUnLike = (postid) => {
+    const config = {
+      method: 'post',
+      url: `${process.env.REACT_APP_NEXTTECH_DEV_URL}/comments/unlike-post`,
+      data: {
+        userid,
+        postid,
+      },
+      headers: {
+        // Add any headers you need here
+      },
+    };
+    axios(config).then((response) => {
+      const data = response.data;
+      console.log('all post', data);
+      setIsLiked(false);
+      handleAllPosts();
+    });
+  };
+
+  const [postLikes, setPostLikes] = useState(0);
+  const [isLiked, setIsLiked] = useState(false);
+
+  const handleLikePost = (postid) => {
+    if (isLiked) {
+      setPostLikes(postLikes - 1);
+      setIsLiked(false);
+      handleUnLike(postid);
+    } else {
+      setPostLikes(postLikes + 1);
+      setIsLiked(true);
+      handleSetLike(postid);
+    }
+  };
 
   return (
     <>
-       <MyPostCardModal modalOpen={modalOpen} handleModalClose={handleModalClose} postId={postId} />
+      <MyPostCardModal modalOpen={modalOpen} handleModalClose={handleModalClose} postId={postId} />
       <div>
         <Typography variant="h5">Posts</Typography>
         {post.map((posts) => (
@@ -81,21 +126,21 @@ const handleLikePost = () => {
               <img src={posts?.image_url} alt="Post" style={{ width: '600px', maxHeight: '600px' }} />
             </CardContent>
             <CardActions disableSpacing>
-              
-            <IconButton color="primary" onClick={() => {handleLikePost();setIsLiked(!isLiked)}}>
-  {isLiked ? (
-    <FavoriteIcon color="error" /> 
-  ) : (
-    <FavoriteBorderOutlinedIcon /> 
-  )}
-</IconButton>
-<Typography>{postLikes} Likes</Typography>
+              <IconButton
+                color="primary"
+                onClick={() => {
+                  handleLikePost(posts._id);
+                }}
+              >
+                {isLiked ? <FavoriteIcon color="error" /> : <FavoriteBorderOutlinedIcon />}
+              </IconButton>
+              <Typography>{posts.number_likes} Likes</Typography>
 
               <IconButton>
                 <Button
                   onClick={() => {
                     handleValue({ id: posts._id });
-                    handleModalOpen(); 
+                    handleModalOpen();
                   }}
                 >
                   <ChatBubbleOutlineIcon />
